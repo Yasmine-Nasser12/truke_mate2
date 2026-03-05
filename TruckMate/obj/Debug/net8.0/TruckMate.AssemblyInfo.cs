e76361818a263671c9e7231ce2c0c42c6b259542
booooody211981-cell <booooody211981@gmail.com>
2026-03-05 22:55:37 +0200

    setup

--- TruckMate/obj/Debug/net8.0/TruckMate.AssemblyInfo.cs
+++ TruckMate/obj/Debug/net8.0/TruckMate.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TruckMate")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4e9dfff41f30660fe3574c53fc12afe9bfe04c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84de7e057e6ae3a20c950ff97107c60569eedeb6")]
 [assembly: System.Reflection.AssemblyProductAttribute("TruckMate")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TruckMate")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
